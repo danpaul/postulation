eec6f06b8364519236e248562ac4d66206a860fa
Dan Breczinski <pt2323@gmail.com>
2016-10-14 08:52:04 -0400

    config update

--- frontend_app/app/config.js
+++ frontend_app/app/config.js
@@ -1,14 +1,16 @@
 var config = {};
 
-config.environment = process.env.NODE_ENV ?
-                     process.env.NODE_ENV : 'development';
+// config.environment = process.env.NODE_ENV ?
+//                      process.env.NODE_ENV : 'development';
 
-config.recordHistory = true;
-config.siteUrl = 'http://localhost:3000';
+// config.recordHistory = true;
+// config.siteUrl = 'http://localhost:3000';
 
-if( config.environment === 'production' ) {
-    config.recordHistory = false;
-    config.siteUrl = 'http://198.199.65.198';
-}
+// if( config.environment === 'production' ) {
+//     config.recordHistory = false;
+//     config.siteUrl = 'http://198.199.65.198';
+// }
+
+config.siteUrl = 'http://198.199.65.198';
 
 module.exports = config;
