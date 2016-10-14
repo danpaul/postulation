b608bc54972de740cf4404c6a7e9dff5f3d3c5dc
Dan Breczinski <pt2323@gmail.com>
2016-10-14 08:57:16 -0400

    config update

--- public/bundle.js
+++ public/bundle.js
@@ -475,8 +475,18 @@
 
 	var config = {};
 
-	config.recordHistory = true;
-	config.siteUrl = 'http://localhost:3000';
+	// config.environment = process.env.NODE_ENV ?
+	//                      process.env.NODE_ENV : 'development';
+
+	// config.recordHistory = true;
+	// config.siteUrl = 'http://localhost:3000';
+
+	// if( config.environment === 'production' ) {
+	//     config.recordHistory = false;
+	//     config.siteUrl = 'http://198.199.65.198';
+	// }
+
+	config.siteUrl = 'http://198.199.65.198';
 
 	module.exports = config;
 
