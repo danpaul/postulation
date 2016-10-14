284b2f74fb763b099b19665c34022753273f62a8
Dan Breczinski <pt2323@gmail.com>
2016-10-14 06:09:55 -0400

    config update

--- frontend_app/app/config.js
+++ frontend_app/app/config.js
@@ -3,4 +3,9 @@ var config = {};
 config.recordHistory = true;
 config.siteUrl = 'http://localhost:3000';
 
+if( config.environment === 'production' ) {
+    config.recordHistory = false;
+    config.siteUrl = 'http://198.199.65.198';
+}
+
 module.exports = config;
